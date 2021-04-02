32607b38caf352c4dde032c249208c53a1f32aa2
Your Name <you@example.com>
2021-04-03 01:25:58 +0800

    加入遍历所有股票的操作
    已知问题:
    1.获取不到最后一天的数据
    2.会出现数据重复问题(比如sh605208)

--- 爬虫/dfcf-cyq.py
+++ 爬虫/dfcf-cyq.py
@@ -7,14 +7,38 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait #等待页面加载某些元素
 import pandas as pd
 import time
+from urllib.request import urlopen
+import json
+import re
+
+# 获取当日所有股票信息:
+# 地址: http://35.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112408020983883791639_1617375883153&pn=1&pz=4000&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1617375883158
+# 说明: pn=1-从哪页开始,pz=4000-获取多少个数据
+def get_list():
+    # 抓取数据
+    url = 'http://35.push2.eastmoney.com/api/qt/clist/get?cb=jQuery112408020983883791639_1617375883153&pn=%s&pz=%s&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:80,m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1617375883158' % (1, 4000)
+    content = urlopen(url=url).read().decode()
+    # print(content)
+    # 解析数据
+    content = re.findall('\((.*)\);', content)  # 寻找(到);之间的内容
+    content = json.loads(content[0])
+    codes = []
+    for info in content['data']['diff']:
+        # f12-代码 f13-地区(0-sz,1-sh) f14-名称
+        # print(info['f12'], info['f13'], info['f14'])
+        code = {'名称':info['f14'], '代码': ('sz' if info['f13'] == 0 else 'sh') + info['f12']}
+        codes.append(code)
+    # print(codes, len(codes), content['data']['total'])
+    return codes
+
 
 # 初始化浏览器
 def browser_init():
     # 无界面模式运行,参考:https://blog.csdn.net/Artificial_idiots/article/details/108490448
     option = webdriver.ChromeOptions()
-    option.add_argument('window-size=1920x3000')  # 指定浏览器分辨率,此数值设置关系到后面的移动距离10.8,变为太小会导致元素遗漏
-    option.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
-    option.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+    # option.add_argument('window-size=1920x3000')  # 指定浏览器分辨率,此数值设置关系到后面的移动距离10.8,变为太小会导致元素遗漏
+    # option.add_argument('--disable-gpu')  # 谷歌文档提到需要加上这个属性来规避bug
+    # option.add_argument('--headless')  # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
     executable_path = r'C:\Program Files\Google\Chrome\Application\chromedriver.exe'
     browser = webdriver.Chrome(executable_path=executable_path, options=option)
     return browser
@@ -76,7 +100,7 @@ def test_data(contents):
     status = -1
     for tmp in contents:
         x.append(tmp[0])
-    if len(set(x)) == len(x) == LEN:
+    if len(set(x)) == len(x):
         status = 0
         print('###########无错误#######')
     else:
@@ -89,71 +113,82 @@ if __name__ == "__main__":
     LEFT, RIGHT, ERROR_CNTS = 40, 20, 10
     STEP = 10.8
 
+    ### 获取今日所有股票列表
+    codes = get_list()
+    print(codes)
+
     ### 打开网页并等待所有元素加载完成
     browser = browser_init()
-    browser.get('http://quote.eastmoney.com/concept/sh603322.html')
-    browser.find_element_by_id('cmfb-btn').click()
-    wait_data(browser)
+    for code in codes:
+        print('正在获取股票%s-%s的数据' % (code['名称'], code['代码']))
+        browser.get('http://quote.eastmoney.com/concept/%s.html' % code['代码'])
+        browser.find_element_by_xpath('//*[@id="type-selector"]/a[5]').click()  # 点击'日K'按钮
+        browser.find_element_by_id('btn-cyq').click()  # 点击'筹码分布'按钮
+        wait_data(browser)
 
-    ### 移动鼠标然后抓取数据
-    # 取得最新交易时间
-    zxsj = browser.find_element_by_xpath('//*[@id="quote-time"]').text[:10]
-    # 移动到柱状图页面,以2为单位逐步向右移动以便抓取数据,避免定位不准导致的重复或者遗漏问题
-    action = ActionChains(browser)
-    action.move_to_element(browser.find_element_by_xpath('//*[@id="chart-container"]/div[1]')).perform()
-    contents_right = []
-    contents_right.append(wait_data(browser))
-    loop_cnts = 0
-    while True:
-        try:
-            action = ActionChains(browser)
-            action.move_by_offset(STEP, 0).perform()
-            content = wait_data(browser)
-            if (len(contents_right) != (RIGHT - 1)) and (content[0] == zxsj):  # 元素未刷新,继续移动
-                continue
-            else:
-                if content not in contents_right:  # 判断是否移动到了新元素
-                    contents_right.append(content)
-            loop_cnts = 0
-        except:
-            loop_cnts = loop_cnts + 1
-        if (len(contents_right) == RIGHT) or (ERROR_CNTS == loop_cnts):  # 判断是否遍历完毕,或者出错过多
-             break
-    print(contents_right)
-    # 移动到柱状图页面,以1为单位逐步向左移动以便抓取数据,避免定位不准导致的重复或者遗漏问题
-    action = ActionChains(browser)
-    action.move_to_element(browser.find_element_by_xpath('//*[@id="chart-container"]/div[1]')).perform()
-    contents_left = []
-    loop_cnts = 0
-    while True:
-        try:
-            action = ActionChains(browser)
-            action.move_by_offset((0 - STEP), 0).perform()
-            content = wait_data(browser)
-            if (content not in contents_left) and \
-                    (content[0] != contents_right[0]) and (content[0] != zxsj):  # 判断是否移动到了新元素
-                contents_left.append(content)
-            loop_cnts = 0
-        except:
-            loop_cnts = loop_cnts + 1
-        if (len(contents_left) == LEFT) or (ERROR_CNTS == loop_cnts):  # 判断是否遍历完毕,或者出错过多
-             break
-    print(contents_left)
+        ### 移动鼠标然后抓取数据
+        # 取得最新交易时间
+        zxsj = browser.find_element_by_xpath('//*[@id="quote-time"]').text[:10]
+        # 移动到柱状图页面,以2为单位逐步向右移动以便抓取数据,避免定位不准导致的重复或者遗漏问题
+        action = ActionChains(browser)
+        action.move_to_element(browser.find_element_by_xpath('//*[@id="chart-container"]/div[1]')).perform()
+        contents_right = []
+        contents_right.append(wait_data(browser))
+        loop_cnts = 0
+        while True:
+            try:
+                action = ActionChains(browser)
+                action.move_by_offset(STEP, 0).perform()
+                content = wait_data(browser)
+                if (len(contents_right) != (RIGHT - 1)) and (content[0] == zxsj):  # 元素未刷新,继续移动
+                    continue
+                else:
+                    if content not in contents_right:  # 判断是否移动到了新元素
+                        contents_right.append(content)
+                loop_cnts = 0
+            except:
+                loop_cnts = loop_cnts + 1
+            if (len(contents_right) == RIGHT) or (ERROR_CNTS == loop_cnts):  # 判断是否遍历完毕,或者出错过多
+                 break
+        # print(contents_right)
+        # 移动到柱状图页面,以1为单位逐步向左移动以便抓取数据,避免定位不准导致的重复或者遗漏问题
+        action = ActionChains(browser)
+        action.move_to_element(browser.find_element_by_xpath('//*[@id="chart-container"]/div[1]')).perform()
+        contents_left = []
+        loop_cnts = 0
+        while True:
+            try:
+                action = ActionChains(browser)
+                action.move_by_offset((0 - STEP), 0).perform()
+                content = wait_data(browser)
+                if (content not in contents_left) and \
+                        (content[0] != contents_right[0]) and (content[0] != zxsj):  # 判断是否移动到了新元素
+                    contents_left.append(content)
+                loop_cnts = 0
+            except:
+                loop_cnts = loop_cnts + 1
+            if (len(contents_left) == LEFT) or (ERROR_CNTS == loop_cnts):  # 判断是否遍历完毕,或者出错过多
+                 break
+        # print(contents_left)
 
-    ### 处理拿到的数据
-    contents = contents_left[::-1] + contents_right
-    print(contents)
-    ret = test_data(contents)
-    print(ret)
-    if ret[0] != 0:
-        exit()
+        ### 处理拿到的数据
+        contents = contents_left[::-1] + contents_right
+        # print(contents)
+        ret = test_data(contents)
+        # print(ret)
+        if ret[0] != 0:
+            exit()
 
-    ### 使用pandas进行处理
-    df = pd.DataFrame(contents)
-    columns = {0: '日期', 1: '获利比例', 2: '平均成本', 3: '90%成本', 4: '集中度', 5: '70%成本', 6: '集中度'}
-    df.rename(columns=columns, inplace=True)
-    print(df)
-    df.to_csv(path_or_buf='cyq.csv', index=False, encoding='gbk')
+        ### 使用pandas进行处理
+        df = pd.DataFrame(contents)
+        columns = {0: '日期', 1: '获利比例', 2: '平均成本', 3: '90%成本', 4: '集中度', 5: '70%成本', 6: '集中度'}
+        df.rename(columns=columns, inplace=True)
+        print(df)
+        df.to_csv(path_or_buf='%s.csv' % code['代码'], index=False, encoding='gbk')
+
+        ### 延迟一下获取下一个股票数据
+        # browser.close()
+        time.sleep(1)
 
     ### 退出
     browser.quit()
